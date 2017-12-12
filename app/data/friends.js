ea693af3d42ff9c335f73d1e916a0a792877cadd
Sam <sjdefrank91@gmail.com>
2017-12-11 18:24:33 -0600

    diversify friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -28,13 +28,13 @@ var friendsList = [
         "photo": "https://static.pexels.com/photos/248797/pexels-photo-248797.jpeg",
         "scores": [5, 4, 4, 3, 4, 2, 3, 3, 2, 3]
     },{
-        "name": "josh",
-        "photo": "none",
-        "scores": [2, 2, 3, 3, 3, 4, 3, 3, 2, 1]
+        "name": "Oscar T. Grouch",
+        "photo": "https://pbs.twimg.com/profile_images/3551544629/30b31e4ef5b587fd54de06f89f5cd571.jpeg",
+        "scores": [1, 5, 5, 3, 1, 5, 5, 4, 5, 1]
     },{
-        "name": "josh",
-        "photo": "none",
-        "scores": [2, 2, 3, 3, 3, 4, 3, 3, 2, 1]
+        "name": "Count Von Count",
+        "photo": "https://camo.githubusercontent.com/bce15c55825bb864fe12e65066880919b8a93b91/687474703a2f2f312e62702e626c6f6773706f742e636f6d2f5f7a434762413550763050492f54476a35596e47454444492f41414141414141414444382f6970594b496763374a67302f733430302f436f756e74566f6e436f756e742e6a7067",
+        "scores": [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]
     },{
         "name": "josh",
         "photo": "none",
